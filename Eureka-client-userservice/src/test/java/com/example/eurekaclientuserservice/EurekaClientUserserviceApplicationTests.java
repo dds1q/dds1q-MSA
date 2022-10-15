a877113c918f2f7f6ada1b419c765468c3982249
dds1q <dom955057@gmail.com>
2022-10-15 21:19:24 +0900

    no message

---
+++ Eureka-client-userservice/src/test/java/com/example/eurekaclientuserservice/EurekaClientUserserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eurekaclientuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientUserserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
