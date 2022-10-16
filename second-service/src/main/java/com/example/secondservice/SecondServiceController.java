b2d12124978311f86a90d0eb1e9e6601b8ad5a8d
dds1q <dom955057@gmail.com>
2022-10-17 02:53:22 +0900

    update check method

--- second-service/src/main/java/com/example/secondservice/SecondServiceController.java
+++ second-service/src/main/java/com/example/secondservice/SecondServiceController.java
@@ -1,5 +1,6 @@
 package com.example.secondservice;
 
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -19,5 +20,10 @@ public class SecondServiceController {
     public String message( @RequestHeader("second-request") String header ){
         log.info( header );
         return "Hello World in Second Service";
+    }
+
+    @GetMapping("/check")
+    public String check(){
+        return "Hi there. This is a message from Second Server";
     }
 }
