9cb507bfb776f3a752495e02f355eace1c21de91
dds1q <dom955057@gmail.com>
2022-10-23 23:41:56 +0900

    catalog kafka consumer 수량 업데이트 기능 추가

--- Ecommerce-Catalog-service/src/main/java/com/example/ecommercecatalogservice/repository/CatalogRepository.java
+++ Ecommerce-Catalog-service/src/main/java/com/example/ecommercecatalogservice/repository/CatalogRepository.java
@@ -1,8 +1,10 @@
 package com.example.ecommercecatalogservice.repository;
 
 import com.example.ecommercecatalogservice.domain.Catalog;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CatalogRepository extends JpaRepository<Catalog, Long> {
 
+    Optional<Catalog> findByProductId(String productId);
 }
